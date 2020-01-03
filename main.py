28deb8193a8d14287e74438afd293aa8c7c91755
Mateusz Kolpa <mateusz.kolpa@wp.pl>
2020-01-03 23:51:45 +0100

    Started working on menu

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 import pygame
 from modules.level import Level
+from modules.button import Button
 
 pygame.init()
 
-tile_size = 50
-board_width, board_height = 20, 20
-window_width = board_width * tile_size + 30
-window_height = board_height * tile_size + tile_size*2 + 45
-window = pygame.display.set_mode((window_width, window_height))
+SCALE = 0.5
+
+TILE_SIZE = int(50 * SCALE)
+BUFF = int(15 * SCALE)
+BOARD_WIDTH, BOARD_HEIGHT = 20, 20
+WINDOW_WIDTH = (BOARD_WIDTH * TILE_SIZE) + BUFF*2
+WINDOW_HEIGHT = (BOARD_HEIGHT * TILE_SIZE) + TILE_SIZE*2 + BUFF*3
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 black = (50, 50, 50)
 border_color = (26, 32, 38)
 score_board_color = (120, 120, 120)
 
-font = pygame.font.SysFont("comicsansms", 72)
+font = pygame.font.SysFont("", int(72*SCALE), bold=True)
 
 clock = pygame.time.Clock()
 
@@ -22,21 +26,21 @@ FPS = 60
 MPS = 5
 
 # Snake head
-snake_head = pygame.Surface((tile_size, tile_size))
+snake_head = pygame.Surface((TILE_SIZE, TILE_SIZE))
 snake_head.fill((50, 200, 50))
 # Snake body
-snake_body = pygame.Surface((tile_size, tile_size))
+snake_body = pygame.Surface((TILE_SIZE, TILE_SIZE))
 snake_body.fill((50, 100, 50))
 # Fruit body
-fruit_body = pygame.Surface((tile_size, tile_size))
+fruit_body = pygame.Surface((TILE_SIZE, TILE_SIZE))
 fruit_body.fill((200, 65, 65))
 # Score board
-score_board = pygame.Surface((board_width * tile_size, tile_size*2))
+score_board = pygame.Surface((BOARD_WIDTH * TILE_SIZE, TILE_SIZE*2))
 
 
-def game():
-    game = Level(board_width, board_height, tile_size, MPS, black, snake_head, snake_body, fruit_body)
-    game_board = pygame.Surface((board_width * tile_size, board_height * tile_size))
+def game_loop():
+    game = Level(BOARD_WIDTH, BOARD_HEIGHT, TILE_SIZE, MPS, black, snake_head, snake_body, fruit_body)
+    game_board = pygame.Surface((BOARD_WIDTH * TILE_SIZE, BOARD_HEIGHT * TILE_SIZE))
     window.fill(border_color)
 
     while True:
@@ -53,8 +57,8 @@ def game():
 
         # Drawing
         game.draw(game_board)
-        window.blit(game_board, (15, 15))
-        window.blit(score_board, (15, board_height*tile_size+30))
+        window.blit(game_board, (BUFF, BUFF))
+        window.blit(score_board, (BUFF, BOARD_HEIGHT*TILE_SIZE+(BUFF*2)))
         score_board.fill(score_board_color)
         score_board.blit(score_text, (score_board.get_width()//2-score_text.get_width()//2, score_text.get_height()//2))
 
@@ -66,18 +70,25 @@ def game():
 
 
 def menu():
+    button_play = Button(x=WINDOW_WIDTH//2-150*SCALE, y=WINDOW_HEIGHT-200*SCALE, width=300*SCALE, height=150*SCALE, text='Play!', font=int(72*SCALE), trigger=game_loop)
+    button_apply = Button(x=WINDOW_WIDTH//2-100*SCALE, y=WINDOW_HEIGHT-400*SCALE, width=200*SCALE, height=100*SCALE, text='Apply', font=int(48*SCALE))
+
     run = True
     while run:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
-                game()
                 run = False
                 exit('-> User quit')
 
         # Logic
+        for button in Button.button_list:
+            button.update(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], pygame.mouse.get_pressed()[0])
 
         # Drawing
         window.fill((255, 255, 255))
+
+        for button in Button.button_list:
+            button.draw(window)
 
         pygame.display.update()
 
---
+++ modules/button.py
@@ -0,0 +1,67 @@
+import pygame
+
+pygame.font.init()
+
+
+class Button:
+    button_list = []
+
+    def __init__(self, x, y, width=100, height=50, colors=((220, 100, 100), (180, 100, 100), (120, 100, 100)),
+                 text="", font=36, font_colors=((220, 220, 220), (180, 220, 220), (120, 180, 180)), trigger=None):
+        self.button_list.append(self)
+
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.colors = colors
+        self.text = text
+        self.font_colors = font_colors
+
+        self.pressed = False
+        self.enabled = True
+        self.visible = True
+
+        # font argument can either be pygame.font.Font or font size
+        if font is pygame.font.Font:
+            self.font = font
+        else:
+            self.font = pygame.font.SysFont("", font)
+
+        self.trigger = trigger
+
+        self.body = pygame.Surface((self.width, self.height))
+        self.update(0, 0, False)
+
+    def update(self, mouse_x, mouse_y, mouse_press):
+        if self.enabled:
+
+            self.pressed = False
+            col = 0
+            if self.is_hovering(mouse_x, mouse_y):
+                self.pressed = mouse_press
+                col = 2 if mouse_press else 1
+
+                if mouse_press and self.trigger is not None:
+                    self.trigger()
+
+            self.body.fill(self.colors[col])
+
+            text_surf = self.font.render(self.text, True, self.font_colors[col])
+            self.body.blit(text_surf, (self.body.get_width()//2-text_surf.get_width()//2,
+                                       self.body.get_height()//2-text_surf.get_height()//2))
+
+    def draw(self, surface):
+        if self.visible:
+            surface.blit(self.body, (self.x, self.y))
+
+    def is_hovering(self, mouse_x, mouse_y):
+        if mouse_x < self.x:
+            return False
+        elif self.x + self.width < mouse_x:
+            return False
+        elif mouse_y < self.y:
+            return False
+        elif self.y + self.height < mouse_y:
+            return False
+        return True
